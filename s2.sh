09fb723ed490b794827aff10adcb5f4a593210d7
Ecoronel06 <ercoronelmo@uide.edu.ec>
2026-05-23 19:08:57 +0000

    wip: basic commands

--- s2.sh
+++ s2.sh
@@ -14,5 +14,21 @@ pwd
 cd 
 #ruta absoluta: es la ruta completa desde el directorio \
 #ruta relati va:no tiene la ruta  completa 
-#lista archivos y directorios 
+#lista archivos y directorios en el directorio actual 
 ls 
+#lista archivos y directorios en el directorio actual
+ #y la opcion -a es all de muestrame todo
+ # incluyendo archivos y directorios ocultos 
+ ls -a
+ #cambio de directorio al directorio raiz de linux 
+ cd /
+ #cambio de directorio al directorio de bianrio  de linux
+ #ruta absoluta 
+ cd /bin 
+ #ruta relativa 
+ cd bin 
+ #este comando sirve para copiar un archivo binario llamado hola_binario
+ #al directorio /bin,usando permiso de administrador (SUDO)
+ sudo cp /workspaces/intro-unix-may-2025-online/hola_binario /bin/hola_binario
+ #make directory creamos   un nuevo directrio 
+ mkdir nuevo_directorio
