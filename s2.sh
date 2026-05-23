1e3fe521713675fbbf71638618e738aa4b8685aa
Ecoronel06 <ercoronelmo@uide.edu.ec>
2026-05-23 18:23:18 +0000

    wip: first comamands and C code

---
+++ s2.sh
@@ -0,0 +1,18 @@
+#Este comando abre el manual completo del compilador gcc
+man gcc
+#Este comando muestra una ayuda rápida del compilador gcc
+gcc --help 
+#con este coamndo compilamos el codigo fuente hola.c 
+gcc hola.c -o hola_binario
+#para ver que tipo de archivo es mi binario 
+file hola_binario
+#nos indica que es un archivo de codigo fuente lenguaje c
+file hola.c
+#imprime el directorio actual 
+pwd 
+#sin ningun argumento me lleva al directorio home 
+cd 
+#ruta absoluta: es la ruta completa desde el directorio \
+#ruta relati va:no tiene la ruta  completa 
+#lista archivos y directorios 
+ls 
